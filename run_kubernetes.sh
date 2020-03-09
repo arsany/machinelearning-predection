5ab1db9a380d7d1c77938000d6d5b9dc47ea34c0
arsany <en.arsany@yahoo.com>
2020-03-09 15:37:44 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=arsanyatya/machinelearning-predection
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --generator=run-pod/v1 --image=$dockerpath  machinelearning-predection
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/machinelearning-predection 8000:80
